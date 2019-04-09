a6f6d560f3d5451902892076b81ab494c8f8bd64
warabanshi <warabanshi@gmail.com>
2019-04-10 00:51:29 +0900

    add some setup process

--- linux/init-env.sh
+++ linux/init-env.sh
@@ -12,3 +12,26 @@ usermod -a -G vboxusers warabanshi
 
 # set secondary language
 yast language set languages=ja_JP
+
+# install google-chrome
+wget https://dl.google.com/linux/linux_signing_key.pub
+rpm --import linux_signing_key.pub
+rm -f linux_signing_key.pub
+
+#-----------------------------------------------------
+# manually install applicatinos
+#-----------------------------------------------------
+
+##  download google-chrome from official page then run below rpm command and install it
+# rpm --install google-chrome-stable_current_x86_64.rpm
+
+##  setup ibus. run below command manually by login user
+# $ ibus-setup
+
+##  install dropbox
+# make ext4 storage 
+# https://blog.techack.net/archive/2018/12/13/opensuse-ad-cal-dropbox
+#
+# after make the storage
+# $ wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
+# $ ~/.dropbox-dist/dropboxd
